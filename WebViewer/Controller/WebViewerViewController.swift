2ea26fe3151e41675f028b2f5561b6aa27a8ec0a
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:18:11 +0800

    Update WebViewerViewController.swift

--- WebViewer/Controller/WebViewerViewController.swift
+++ WebViewer/Controller/WebViewerViewController.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerViewController.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 import UIKit
 import WebKit
