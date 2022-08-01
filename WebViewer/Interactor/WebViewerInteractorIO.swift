a940f9fe84ce586f285f6e226e3d5a37fe8b8ca6
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:19:30 +0800

    Update WebViewerInteractorIO.swift

--- WebViewer/Interactor/WebViewerInteractorIO.swift
+++ WebViewer/Interactor/WebViewerInteractorIO.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerInteractorIO.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 protocol WebViewerInteractorInterface {
 
