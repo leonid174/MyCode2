2cbeb4142639946c58d83c723a311e7e357d5f49
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:19:51 +0800

    Update WebViewerModel.swift

--- WebViewer/Model/WebViewerModel.swift
+++ WebViewer/Model/WebViewerModel.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerModel.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 struct WebViewerModel {
 
