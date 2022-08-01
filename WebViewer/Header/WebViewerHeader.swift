0d4a9c4664335fb0f6b9134122890ac07afd431f
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:18:34 +0800

    Update WebViewerHeader.swift

--- WebViewer/Header/WebViewerHeader.swift
+++ WebViewer/Header/WebViewerHeader.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerHeader.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 import UIKit
 
