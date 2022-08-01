26de70bc7537e633f98478b83b2f41b1be8c29f1
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:20:15 +0800

    Update WebViewerPresenter.swift

--- WebViewer/Presenter/WebViewerPresenter.swift
+++ WebViewer/Presenter/WebViewerPresenter.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerPresenter.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 final class WebViewerPresenter {
     var interactor: WebViewerInteractorInterface?
