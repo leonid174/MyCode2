00cb323068b29d4ed8289b6d7c229cce0afb5241
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:19:14 +0800

    Update WebViewerInteractor.swift

--- WebViewer/Interactor/WebViewerInteractor.swift
+++ WebViewer/Interactor/WebViewerInteractor.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerInteractor.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 final class WebViewerInteractor {
     weak var presenter: WebViewerInteractorDelegate?
