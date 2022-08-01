be110b7045bf1d808f6d4d450d9570cfaea1700d
leonid174 <55400550+leonid174@users.noreply.github.com>
2022-08-01 22:18:53 +0800

    Update WebViewerIO.swift

--- WebViewer/Header/WebViewerIO.swift
+++ WebViewer/Header/WebViewerIO.swift
@@ -1,10 +1,4 @@
-//
-//  WebViewerWebViewerIO.swift
-//  RemotePhotoshoot
-//
-//  Created by Dmitry Ponomarev on 17/06/2020.
-//  Copyright 2020 Unoporoduction. All rights reserved.
-//
+
 
 protocol WebViewerInterface {
 
